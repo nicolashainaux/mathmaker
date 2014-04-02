53df238cc46dae4a6fd1196083e143cc6f6f520d
Nicolas Hainaux <nh.techn@gmail.com>
2014-04-02 21:35:43 +0200

    First version of Q_MentalCalculation.py

--- sheet/exercise/question/Q_MentalCalculation.py
+++ sheet/exercise/question/Q_MentalCalculation.py
@@ -22,22 +22,17 @@
 
 from lib import *
 from .Q_Structure import Q_Structure
+from . import mc_modules
 
-from core.base_calculus import *
+#from core.base_calculus import *
 
-# The matching number indicates how many different questions can be asked
-# in the theme
-GAUGES = {'table_2-9' : 36,
-          'table_11' : 28, # 11×n where 10 < n < 100, no carry over
-          'table_15' : 6, # 15×n where 2 <= n <= 6
-          'table_25' : 6 # 25×n where 2 <= n <= 6
-         }
 
 AVAILABLE_Q_KIND_VALUES = \
-    {{'multiplication', 'direct'} : ['table_2-9',
-                                     'table_11',
-                                     'table_15',
-                                     'table_25'],
+    {{'multiplication', 'direct'} :\
+            ['table_2-9',
+             'table_11',  # 11×n where 10 < n < 100, no carry over
+             'table_15',  # 15×n where 2 <= n <= 6
+             'table_25'], # 25×n where 2 <= n <= 6
      {'multiplication', 'reversed'} : ['table_2-9'],
      {'multiplication', 'decimal'} : ['table_2-9'],
      {'multiplication', 'decimal_1'} : ['table_2-9'],
@@ -52,10 +47,23 @@ AVAILABLE_Q_KIND_VALUES = \
                                                  'table_11',
                                                  'table_15',
                                                  'table_25'],
-     {'area', 'right_rectangle'} : ['table_2-9',
-                                    'table_11',
-                                    'table_15',
-                                    'table_25']
+     {'area', 'right_triangle'} : ['table_2-9',
+                                   'table_11',
+                                   'table_15',
+                                   'table_25']
+    }
+
+MODULES =  \
+    {{'multiplication', 'direct'} : multi_dir,
+     {'multiplication', 'reversed'} : multi_rev,
+     {'multiplication', 'decimal'} : multi_deci,
+     {'multiplication', 'decimal_1'} : multi_deci1,
+     {'multiplication', 'decimal_2'} : multi_deci2,
+     {'division', 'direct'} : divi_dir,
+     {'division', 'decimal_1'} : divi_deci1,
+     {'area', 'rectangle', 'with_drawing'} : area_rect_dr,
+     {'area', 'rectangle', 'without_drawing'} : area_rect_no_dr,
+     {'area', 'right_triangle'} : area_right_tri
     }
 
 
@@ -137,98 +145,13 @@ class Q_MentalCalculation(Q_Structure):
         # value of **options
         options = self.options
 
-        self.calculus_list = []
-        self.results_list = []
+        # module
+        m = mc_modules.MODULES[self.q_kind]
 
-        if q_kind == '10m_2-9':
-            # The following intension list creates all (i , j) couples, i and
-            # j taking their values in 2-9 and j >= i to avoid having duplicates
-            # like (3, 2) which would be the same as (2, 3)
-            # pairs = [(i, j) for i in xrange(2, 10) for j in xrange(i, 10)]
-            # We put off 2×2 and 2×3
-            pairs =
+        self.numbers_to_use = m.mix(options['numbers_to_use'])
+        self.q_text = m.q(embedded_machine, self.numbers_to_use)
+        self.q_answer = m.a(embedded_machine, self.numbers_to_use)
 
-            (self.calculus_list, self.results_list) = ten_products(pairs)
-
-        elif q_kind == '10m_4-9':
-            # The following intension list creates all (i , j) couples, i and
-            # j taking their values in 4-9 and j >= i to avoid having duplicates
-            # like (5, 6) which would be the same as (6, 5)
-            # pairs = [(i, j) for i in xrange(4, 10) for j in xrange(i, 10)]
-            pairs = [(4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9),
-                     (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (6, 6),
-                     (6, 7), (6, 8), (6, 9), (7, 7), (7, 8), (7, 9),
-                     (8, 8), (8, 9), (9, 9)]
-
-
-            (self.calculus_list, self.results_list) = ten_products(pairs)
-
-
-        elif q_kind == '5m_3rm_2d_2-9':
-            # The following intension list creates all (i , j) couples, i and
-            # j take their values in 2 - 9 and j >= i to avoid having duplicates
-            # like (3, 2) which would be the same as (2, 3)
-            # pairs = [(i, j) for i in xrange(2, 10) for j in xrange(i, 10)]
-            # We put off 2×2 and 2×3
-            pairs = [(2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9),
-                     (3, 3), (3, 4), (3, 5), (3, 6), (3, 7), (3, 8), (3, 9),
-                     (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9),
-                     (5, 5), (5, 6), (5, 7), (5, 8), (5, 9),
-                     (6, 6), (6, 7), (6, 8), (6, 9),
-                     (7, 7), (7, 8), (7, 9),
-                     (8, 8), (8, 9),
-                     (9, 9)]
-
-            for i in range(5):
-                current_pair = randomly.pop(pairs)
-                if randomly.heads_or_tails():
-                    self.calculus_list.append(Product([current_pair[0],
-                                                       current_pair[1]]))
-                else:
-                    self.calculus_list.append(Product([current_pair[1],
-                                                       current_pair[0]]))
-
-                self.results_list.append(Product([current_pair[0],
-                                                  current_pair[1]]).evaluate()
-                                                  )
-
-            for i in range(3):
-                current_pair = randomly.pop(pairs)
-                self.calculus_list.append(Product([current_pair[0],
-                                                  current_pair[1]]).evaluate()
-                                                  )
-
-                self.results_list.append(Product([current_pair[0],
-                                                  current_pair[1]]))
-
-            # We put (2, 2) and (2, 3) back in the possible pairs
-            pairs.append((2, 2))
-            pairs.append((2, 3))
-
-            for i in range(2):
-                current_pair = randomly.pop(pairs)
-
-                if randomly.heads_or_tails():
-                    quotient = Quotient(('+',
-                                         Product([current_pair[0],
-                                                  current_pair[1]]).evaluate(),
-                                         current_pair[0]),
-                                         use_divide_symbol='yes')
-
-                    self.calculus_list.append(quotient)
-
-                    self.results_list.append(current_pair[1])
-
-                else:
-                    quotient = Quotient(('+',
-                                         Product([current_pair[0],
-                                                  current_pair[1]]).evaluate(),
-                                         current_pair[1]),
-                                         use_divide_symbol='yes')
-
-                    self.calculus_list.append(quotient)
-
-                    self.results_list.append(current_pair[0])
 
 
 
@@ -242,27 +165,7 @@ class Q_MentalCalculation(Q_Structure):
     ##
     #   @brief Returns the text of the question as a str
     def text_to_str(self):
-        M = self.machine
-
-        result = M.write(_("Date") + " : ............." \
-                         + _("Class") + " : .............")
-
-        result += M.write_new_line()
-
-        size = (3, 10)
-        col_widths = [1.4 for i in range(10)]
-        content = [i+1 for i in range(10)] \
-                  + [M.write_math_style2(\
-                    M.type_string(self.calculus_list[i])) for i in range(10)] \
-                  + [M.write_math_style2(\
-                    M.type_string(self.results_list[i])) for i in range(10)]
-
-        result += M.write_layout(size, col_widths, content,
-                                borders='all', center='ok')
-
-        result += M.write_new_line_twice()
-
-        return result
+        return self.q_text
 
 
 
@@ -275,7 +178,7 @@ class Q_MentalCalculation(Q_Structure):
     ##
     #   @brief Returns the answer of the question as a str
     def answer_to_str(self):
-        pass
+        return self.q_answer
 
 
 
