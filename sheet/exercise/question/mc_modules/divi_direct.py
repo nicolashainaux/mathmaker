b29bc85f45f707db00262837b64ed005616ef378
Nicolas Hainaux <nh.techn@gmail.com>
2015-11-24 17:31:02 +0100

    Corrected an error in divi_direct.py

--- sheet/exercise/question/mc_modules/divi_direct.py
+++ sheet/exercise/question/mc_modules/divi_direct.py
@@ -30,7 +30,7 @@ class sub_object(object):
         self.divisor = 0
         self.result = 0
         self.dividend = 0
-        
+
         if '10_100_1000' in options and options['10_100_1000']:
             self.divisor = nb_list[0]
             self.dividend = nb_list[1]
@@ -39,9 +39,9 @@ class sub_object(object):
                           .into_str(force_expression_begins=True)
         else:
             self.divisor = randomly.pop(nb_list)
-            self.result = Item(randomly.pop(nb_list))\
-                          .into_str(force_expression_begins=True)
+            self.result = Item(randomly.pop(nb_list))
             self.dividend = Product([self.divisor, self.result]).evaluate()
+            self.result = self.result.into_str(force_expression_begins=True)
 
         self.quotient = Quotient(('+', self.dividend, self.divisor),
                                  use_divide_symbol=True)
@@ -49,8 +49,7 @@ class sub_object(object):
     def q(self, M, **options):
         return _("Calculate:") + " "\
                + M.write_math_style2(self.quotient\
-                                     .into_str(force_expression_begins=True)
-                                    )
+                                     .into_str(force_expression_begins=True))
 
     def a(self, M, **options):
         return M.write_math_style2(self.result)
