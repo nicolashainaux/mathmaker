d5b0f3a138ecf7633130c971afcf7be809b08c4c
Nicolas Hainaux <nh.techn@gmail.com>
2016-03-13 08:14:31 +0100

    Added a first version of unique_letters_words.py

---
+++ lib/common/unique_letters_words.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker creates automatically maths exercises sheets
+# with their answers
+# Copyright 2006-2015 Nicolas Hainaux <nico_h@users.sourceforge.net>
+
+# This file is part of Mathmaker.
+
+# Mathmaker is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+FOUR_LETTERS_WORDS = { 'fr_FR': ["ABRI", "ACTE", "AGIR", "AIDE", "AIGU", "ALTO", "AMER", "ANGE", "ANIS", "AOUT", "APRE", "APTE", "ARDU", "ARME", "ASIE", "AUBE", "AUTO", "AVEN", "AZUR"],
+					   'en' : ["LEFT", "LAMP", "ATOM", "GIRL", "PLUM", "NAVY", "GIFT", "DARK"] }
+
+FOUR_LETTERS_WORDS['fr'] = FOUR_LETTERS_WORDS['fr_FR']
