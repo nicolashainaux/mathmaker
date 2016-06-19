218ebfbbee21c2a11691bf707f6c3bd54b9da41c
Nicolas Hainaux <nh.techn@gmail.com>
2016-06-19 13:15:02 +0200

    Compiling mo files automatically at build time. Removed them from git's scope.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import subprocess
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
@@ -29,6 +31,26 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
+
+def create_mo_files():
+    data_files = []
+    localedir = 'mathmaker/locale'
+    po_dirs = [localedir + '/' + l + '/LC_MESSAGES/'
+               for l in next(os.walk(localedir))[1]]
+    for d in po_dirs:
+        mo_files = []
+        po_files = [f
+                    for f in next(os.walk(d))[2]
+                    if os.path.splitext(f)[1] == '.po']
+        for po_file in po_files:
+            filename, extension = os.path.splitext(po_file)
+            mo_file = filename + '.mo'
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(d + po_file, d + mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo_files.append(d + mo_file)
+        data_files.append((d, mo_files))
+    return data_files
+
 setup(
     name=mathmaker.lib.__software_name__,
     version=mathmaker.lib.__version__,
@@ -44,9 +66,11 @@ setup(
                 'and their (detailed) answers.',
     long_description=read('README.rst', 'CHANGELOG.rst'),
     packages=find_packages(exclude=['tests']),
+    #    package_dir={'mathmaker': 'mathmaker'},
     entry_points={
         'console_scripts': ['mathmaker = mathmaker.cli:entry_point'],
     },
+    data_files=create_mo_files(),
     include_package_data=True,
     platforms='any',
     test_suite='tests',
