dbcba8777510b5421b66891d22c4b9e87033516c
Nicolas Hainaux <nh.techn@gmail.com>
2016-06-20 18:09:40 +0200

    Updated the scripts of mathmaker/tools/

--- tools/build_db.py
+++ tools/build_db.py
@@ -21,25 +21,19 @@
 # along with Mathmaker; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import sys, os, inspect
+import os
 import sqlite3
-import argparse
 
-current_dir = os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda:0)))
-sys.path.insert(0, current_dir[:current_dir.rfind(os.path.sep)])
-import settings
-from lib.tools import po_file, xml_sheet
-sys.path.pop(0)
-os.chdir('..')
+from mathmaker import settings
+from mathmaker.lib.tools import po_file, xml_sheet
 settings.init()
-from settings import CONFIG
 
 WORDINGS_DIR = settings.datadir + "wordings/"
-WORDINGS_FILES = [ WORDINGS_DIR + n + ".xml"\
-                   for n in ["mini_pb_addi_direct",
-                             "mini_pb_divi_direct",
-                             "mini_pb_subtr_direct",
-                             "mini_pb_multi_direct"] ]
+WORDINGS_FILES = [WORDINGS_DIR + n + ".xml"
+                  for n in ["mini_pb_addi_direct",
+                            "mini_pb_divi_direct",
+                            "mini_pb_subtr_direct",
+                            "mini_pb_multi_direct"]]
 
 # Existent db is deleted. A brand new empty db is created.
 if os.path.isfile(settings.path.db_dist):
@@ -69,55 +63,56 @@ for lang in next(os.walk(settings.localedir))[1]:
     settings.language = lang
     if os.path.isfile(settings.localedir + lang + "/LC_MESSAGES/w4l.po"):
         words = po_file.get_list_of('words', lang, 4)
-        db_rows = list( zip([lang for _ in range(len(words))],
-                            words,
-                            [0 for _ in range(len(words))]))
-        db.executemany(\
-                    "INSERT INTO w4l(language, word, drawDate) VALUES(?, ?, ?)",
-                    db_rows)
+        db_rows = list(zip([lang for _ in range(len(words))],
+                           words,
+                           [0 for _ in range(len(words))]))
+        db.executemany(
+            "INSERT INTO w4l(language, word, drawDate) VALUES(?, ?, ?)",
+            db_rows)
 
     for gender in ["masculine", "feminine"]:
-        if os.path.isfile(settings.localedir + lang \
+        if os.path.isfile(settings.localedir + lang
                           + "/LC_MESSAGES/" + gender + "_names.po"):
             # __
             names = po_file.get_list_of('names', lang, gender)
             db_rows = list(zip([lang for _ in range(len(names))],
-                                [gender for _ in range(len(names))],
-                                names,
-                                [0 for _ in range(len(names))]))
-            db.executemany("INSERT "\
-                           "INTO names(language, gender, name, drawDate) "\
+                               [gender for _ in range(len(names))],
+                               names,
+                               [0 for _ in range(len(names))]))
+            db.executemany("INSERT "
+                           "INTO names(language, gender, name, drawDate) "
                            "VALUES(?, ?, ?, ?)",
                            db_rows)
 
 # Extract data from xml files and insert them into the db
 for f in WORDINGS_FILES:
     wordings = xml_sheet.get_attributes(f, "wording")
-    db_rows = list( zip([w['wording_context'] for w in wordings],
-                        [w['wording'] for w in wordings],
-                        [w['nb1_min'] for w in wordings],
-                        [w['nb1_max'] for w in wordings],
-                        [w['nb2_min'] for w in wordings],
-                        [w['nb2_max'] for w in wordings],
-                        [w['q_id'] for w in wordings],
-                        [0 for _ in range(len(wordings))]))
-    db.executemany("INSERT "\
-                   "INTO mini_pb_wordings(wording_context, wording, "\
-                   "nb1_min, nb1_max, nb2_min, nb2_max, "\
-                   "q_id, drawDate) "\
+    db_rows = list(zip([w['wording_context'] for w in wordings],
+                       [w['wording'] for w in wordings],
+                       [w['nb1_min'] for w in wordings],
+                       [w['nb1_max'] for w in wordings],
+                       [w['nb2_min'] for w in wordings],
+                       [w['nb2_max'] for w in wordings],
+                       [w['q_id'] for w in wordings],
+                       [0 for _ in range(len(wordings))]))
+    db.executemany("INSERT "
+                   "INTO mini_pb_wordings(wording_context, wording, "
+                   "nb1_min, nb1_max, nb2_min, nb2_max, "
+                   "q_id, drawDate) "
                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
                    db_rows)
 
 # Insert integers pairs into the db
 # Tables of 2, 3... 99
-db_rows = [(i+2, j+2, 0, 0) for i in range(199) for j in range(199) if j >= i]
-db.executemany("INSERT "\
-               "INTO int_pairs(nb1, nb2, "\
-               "multirev_locked, drawDate) "\
+db_rows = [(i + 2, j + 2, 0, 0)
+           for i in range(199)
+           for j in range(199)
+           if j >= i]
+db.executemany("INSERT "
+               "INTO int_pairs(nb1, nb2, "
+               "multirev_locked, drawDate) "
                "VALUES(?, ?, ?, ?)",
                db_rows)
 
-
 db.commit()
 db.close()
-
