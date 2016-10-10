297a556be9b008d517970eccc890a55bfdf5cdb9
Nicolas Hainaux <nh.techn@gmail.com>
2016-10-10 14:22:41 +0200

    Add w5l entries into database. Plus: build_db.py is not executed as import (and includes the test for w4l and w5l words); add clever pairs entries, and single int entries.

--- tools/build_db.py
+++ tools/build_db.py
@@ -29,6 +29,7 @@ It will add all entries:
 - from files mini_pb_addi_direct.xml,mini_pb_divi_direct.xml,
   mini_pb_subtr_direct.xml and mini_pb_multi_direct.xml from data/wordings/,
 - from all w4l.po files from locale/*/LC_MESSAGES/
+- from all w5l.po files from locale/*/LC_MESSAGES/
 - from all *_names.po files from locale/*/LC_MESSAGES/
 - all single ints from 2 to 500
 - all integers pairs from 2 to 500
@@ -40,136 +41,153 @@ import sqlite3
 
 from mathmaker import settings
 from mathmaker.lib.tools import po_file, xml_sheet
-settings.init()
+from mathmaker.lib.tools.auxiliary_functions import check_unique_letters_words
 
-WORDINGS_DIR = settings.datadir + "wordings/"
-WORDINGS_FILES = [WORDINGS_DIR + n + ".xml"
-                  for n in ["mini_pb_addi_direct",
-                            "mini_pb_divi_direct",
-                            "mini_pb_subtr_direct",
-                            "mini_pb_multi_direct"]]
 
-# Existent db is deleted. A brand new empty db is created.
-if os.path.isfile(settings.path.db_dist):
-    os.remove(settings.path.db_dist)
-open(settings.path.db_dist, 'a').close()
-db = sqlite3.connect(settings.path.db_dist)
+def __main__():
+    settings.init()
 
-# Creation of the tables
-db.execute('''CREATE TABLE w4l
-          (id INTEGER PRIMARY KEY,
-          language TEXT, word TEXT, drawDate INTEGER)''')
-db.execute('''CREATE TABLE names
-          (id INTEGER PRIMARY KEY,
-          language TEXT, gender TEXT, name TEXT, drawDate INTEGER)''')
-db.execute('''CREATE TABLE mini_pb_wordings
-          (id INTEGER PRIMARY KEY,
-          wording_context TEXT, wording TEXT,
-          nb1_min INTEGER, nb1_max INTEGER, nb2_min INTEGER, nb2_max INTEGER,
-          q_id TEXT, drawDate INTEGER)''')
-db.execute('''CREATE TABLE single_ints
-          (id INTEGER PRIMARY KEY, nb1 INTEGER, drawDate INTEGER)''')
-db.execute('''CREATE TABLE int_pairs
-          (id INTEGER PRIMARY KEY,
-          nb1 INTEGER, nb2 INTEGER,
-          multirev_locked INTEGER, drawDate INTEGER,
-          clever INTEGER)''')
-# As int_deci_clever_pairs may be 'unioned' with int_pairs, its ids will be
-# determined starting from the max id of int_pairs, in order to have unique
-# ids over the two tables.
-db.execute('''CREATE TABLE int_deci_clever_pairs
-          (id INTEGER,
-          nb1 FLOAT, nb2 FLOAT,
-          drawDate INTEGER,
-          clever INTEGER)''')
+    WORDINGS_DIR = settings.datadir + "wordings/"
+    WORDINGS_FILES = [WORDINGS_DIR + n + ".xml"
+                      for n in ["mini_pb_addi_direct",
+                                "mini_pb_divi_direct",
+                                "mini_pb_subtr_direct",
+                                "mini_pb_multi_direct"]]
 
-# Extract data from po(t) files and insert them into the db
-for lang in next(os.walk(settings.localedir))[1]:
-    settings.language = lang
-    if os.path.isfile(settings.localedir + lang + "/LC_MESSAGES/w4l.po"):
-        words = po_file.get_list_of('words', lang, 4)
-        db_rows = list(zip([lang for _ in range(len(words))],
-                           words,
-                           [0 for _ in range(len(words))]))
-        db.executemany(
-            "INSERT INTO w4l(language, word, drawDate) VALUES(?, ?, ?)",
-            db_rows)
+    # Existent db is deleted. A brand new empty db is created.
+    if os.path.isfile(settings.path.db_dist):
+        os.remove(settings.path.db_dist)
+    open(settings.path.db_dist, 'a').close()
+    db = sqlite3.connect(settings.path.db_dist)
 
-    for gender in ["masculine", "feminine"]:
-        if os.path.isfile(settings.localedir + lang
-                          + "/LC_MESSAGES/" + gender + "_names.po"):
-            # __
-            names = po_file.get_list_of('names', lang, gender)
-            db_rows = list(zip([lang for _ in range(len(names))],
-                               [gender for _ in range(len(names))],
-                               names,
-                               [0 for _ in range(len(names))]))
-            db.executemany("INSERT "
-                           "INTO names(language, gender, name, drawDate) "
-                           "VALUES(?, ?, ?, ?)",
-                           db_rows)
+    # Creation of the tables
+    db.execute('''CREATE TABLE w4l
+              (id INTEGER PRIMARY KEY,
+              language TEXT, word TEXT, drawDate INTEGER)''')
+    db.execute('''CREATE TABLE w5l
+              (id INTEGER PRIMARY KEY,
+              language TEXT, word TEXT, drawDate INTEGER)''')
+    db.execute('''CREATE TABLE names
+              (id INTEGER PRIMARY KEY,
+              language TEXT, gender TEXT, name TEXT, drawDate INTEGER)''')
+    db.execute('''CREATE TABLE mini_pb_wordings
+              (id INTEGER PRIMARY KEY,
+              wording_context TEXT, wording TEXT,
+              nb1_min INTEGER, nb1_max INTEGER,
+              nb2_min INTEGER, nb2_max INTEGER,
+              q_id TEXT, drawDate INTEGER)''')
+    db.execute('''CREATE TABLE single_ints
+              (id INTEGER PRIMARY KEY, nb1 INTEGER, drawDate INTEGER)''')
+    db.execute('''CREATE TABLE int_pairs
+              (id INTEGER PRIMARY KEY,
+              nb1 INTEGER, nb2 INTEGER,
+              multirev_locked INTEGER, drawDate INTEGER,
+              clever INTEGER)''')
+    # As int_deci_clever_pairs may be 'unioned' with int_pairs, its ids will be
+    # determined starting from the max id of int_pairs, in order to have unique
+    # ids over the two tables.
+    db.execute('''CREATE TABLE int_deci_clever_pairs
+              (id INTEGER,
+              nb1 FLOAT, nb2 FLOAT,
+              drawDate INTEGER,
+              clever INTEGER)''')
 
-# Extract data from xml files and insert them into the db
-for f in WORDINGS_FILES:
-    wordings = xml_sheet.get_attributes(f, "wording")
-    db_rows = list(zip([w['wording_context'] for w in wordings],
-                       [w['wording'] for w in wordings],
-                       [w['nb1_min'] for w in wordings],
-                       [w['nb1_max'] for w in wordings],
-                       [w['nb2_min'] for w in wordings],
-                       [w['nb2_max'] for w in wordings],
-                       [w['q_id'] for w in wordings],
-                       [0 for _ in range(len(wordings))]))
+    # Extract data from po(t) files and insert them into the db
+    for lang in next(os.walk(settings.localedir))[1]:
+        settings.language = lang
+        for n in [4, 5]:
+            if os.path.isfile(settings.localedir + lang
+                              + "/LC_MESSAGES/w{}l.po".format(str(n))):
+                words = po_file.get_list_of('words', lang, n)
+                check_unique_letters_words(words, n)
+                db_rows = list(zip([lang for _ in range(len(words))],
+                                   words,
+                                   [0 for _ in range(len(words))]))
+                db.executemany(
+                    "INSERT INTO w{}l(language, word, drawDate) "
+                    "VALUES(?, ?, ?)".format(str(n)),
+                    db_rows)
+
+        for gender in ["masculine", "feminine"]:
+            if os.path.isfile(settings.localedir + lang
+                              + "/LC_MESSAGES/" + gender + "_names.po"):
+                # __
+                names = po_file.get_list_of('names', lang, gender)
+                db_rows = list(zip([lang for _ in range(len(names))],
+                                   [gender for _ in range(len(names))],
+                                   names,
+                                   [0 for _ in range(len(names))]))
+                db.executemany("INSERT "
+                               "INTO names(language, gender, name, drawDate) "
+                               "VALUES(?, ?, ?, ?)",
+                               db_rows)
+
+    # Extract data from xml files and insert them into the db
+    for f in WORDINGS_FILES:
+        wordings = xml_sheet.get_attributes(f, "wording")
+        db_rows = list(zip([w['wording_context'] for w in wordings],
+                           [w['wording'] for w in wordings],
+                           [w['nb1_min'] for w in wordings],
+                           [w['nb1_max'] for w in wordings],
+                           [w['nb2_min'] for w in wordings],
+                           [w['nb2_max'] for w in wordings],
+                           [w['q_id'] for w in wordings],
+                           [0 for _ in range(len(wordings))]))
+        db.executemany("INSERT "
+                       "INTO mini_pb_wordings(wording_context, wording, "
+                       "nb1_min, nb1_max, nb2_min, nb2_max, "
+                       "q_id, drawDate) "
+                       "VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+                       db_rows)
+
+    # Insert integers pairs into the db
+    # Tables of 2, 3... 500
+    db_rows = [(i + 2, j + 2, 0, 0, 0)
+               for i in range(499)
+               for j in range(499)
+               if j >= i]
     db.executemany("INSERT "
-                   "INTO mini_pb_wordings(wording_context, wording, "
-                   "nb1_min, nb1_max, nb2_min, nb2_max, "
-                   "q_id, drawDate) "
-                   "VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+                   "INTO int_pairs(nb1, nb2, multirev_locked, drawDate, "
+                   "clever) "
+                   "VALUES(?, ?, ?, ?, ?)",
                    db_rows)
 
-# Insert integers pairs into the db
-# Tables of 2, 3... 500
-db_rows = [(i + 2, j + 2, 0, 0, 0)
-           for i in range(499)
-           for j in range(499)
-           if j >= i]
-db.executemany("INSERT "
-               "INTO int_pairs(nb1, nb2, multirev_locked, drawDate, clever) "
-               "VALUES(?, ?, ?, ?, ?)",
-               db_rows)
+    for couple in [(2, 5), (2, 50), (2, 500), (5, 20), (5, 200)]:
+        db.execute("UPDATE int_pairs SET clever = 5"
+                   + " WHERE nb1 = '" + str(couple[0])
+                   + "' and nb2 = '" + str(couple[1]) + "';")
 
-for couple in [(2, 5), (2, 50), (2, 500), (5, 20), (5, 200)]:
-    db.execute("UPDATE int_pairs SET clever = 5"
-               + " WHERE nb1 = '" + str(couple[0])
-               + "' and nb2 = '" + str(couple[1]) + "';")
+    for couple in [(4, 25), (4, 250)]:
+        db.execute("UPDATE int_pairs SET clever = 4"
+                   + " WHERE nb1 = '" + str(couple[0])
+                   + "' and nb2 = '" + str(couple[1]) + "';")
 
-for couple in [(4, 25), (4, 250)]:
-    db.execute("UPDATE int_pairs SET clever = 4"
-               + " WHERE nb1 = '" + str(couple[0])
-               + "' and nb2 = '" + str(couple[1]) + "';")
+    # Insert integer/decimal "clever" pairs into the db
+    # The tenths series (only one yet) is identified by a 10
+    # the quarters series by a 4
+    # the halfs/fifths series by a 5
+    start_id = tuple(db.execute("SELECT MAX(id) FROM int_pairs "))[0][0] + 1
 
-# Insert integer/decimal "clever" pairs into the db
-# The tenths series (only one yet) is identified by a 10
-# the quarters series by a 4
-# the halfs/fifths series by a 5
-start_id = tuple(db.execute("SELECT MAX(id) FROM int_pairs "))[0][0] + 1
+    db_rows = list(zip([i + start_id for i in range(5)],
+                       [0.2, 2, 4, 4, 0.1],
+                       [5, 0.5, 0.25, 2.5, 10],
+                       [0, 0, 0, 0, 0],
+                       [5, 5, 4, 4, 10]))
 
-db_rows = list(zip([i + start_id for i in range(5)],
-                   [0.2, 2, 4, 4, 0.1],
-                   [5, 0.5, 0.25, 2.5, 10],
-                   [0, 0, 0, 0, 0],
-                   [5, 5, 4, 4, 10]))
+    db.executemany("INSERT "
+                   "INTO int_deci_clever_pairs(id, nb1, nb2, drawDate, "
+                   "clever) "
+                   "VALUES(?, ?, ?, ?, ?)",
+                   db_rows)
 
-db.executemany("INSERT "
-               "INTO int_deci_clever_pairs(id, nb1, nb2, drawDate, clever) "
-               "VALUES(?, ?, ?, ?, ?)",
-               db_rows)
+    # Single ints
+    db_rows = [(i + 1, 0) for i in range(500)]
+    db.executemany("INSERT "
+                   "INTO single_ints(nb1, drawDate) "
+                   "VALUES(?, ?)",
+                   db_rows)
+    db.commit()
+    db.close()
 
-# Single ints
-db_rows = [(i + 1, 0) for i in range(500)]
-db.executemany("INSERT "
-               "INTO single_ints(nb1, drawDate) "
-               "VALUES(?, ?)",
-               db_rows)
-db.commit()
-db.close()
+if __name__ == '__main__':
+    __main__()
