a882a3e92457600414519c9e30a805f7ff532be3
Nicolas Hainaux <nh.techn@gmail.com>
2016-10-13 14:57:32 +0200

    Add some tests.

--- tests/01_core_objects/test_110_polygons.py
+++ tests/01_core_objects/test_110_polygons.py
@@ -20,13 +20,15 @@
 # along with Mathmaker; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import pytest
+
 from mathmaker.lib.core.root_calculus import Value
 from mathmaker.lib.core.base_geometry import Point
 from mathmaker.lib.core.geometry import Polygon
 
 
-def test_p1_into_euk():
-    """Check Polygon's generated euk file."""
+@pytest.fixture
+def p1():
     p1 = Polygon([Point(["A", (0.5, 0.5)]),
                   Point(["B", (3, 1)]),
                   Point(["C", (3.2, 4)]),
@@ -44,6 +46,11 @@ def test_p1_into_euk():
     p1.angle[1].mark = 'simple'
     p1.angle[2].mark = 'simple'
     p1.angle[3].mark = 'simple'
+    return p1
+
+
+def test_p1_into_euk(p1):
+    """Check Polygon's generated euk file."""
     assert p1.into_euk() == \
         'box -0.1, -0.1, 3.8, 4.6\n\n'\
         'A = point(0.5, 0.5)\n'\
@@ -71,4 +78,45 @@ def test_p1_into_euk():
         '  C, B, A simple\n'\
         '  D, C, B simple\n'\
         '  A, D, C simple\n'\
+        'end'
+
+
+def test_p1_rename_errors(p1):
+    """Check wrong arguments trigger exceptions when renaming."""
+    with pytest.raises(TypeError):
+        p1.rename(5678)
+    with pytest.raises(ValueError):
+        p1.rename('KJLIZ')
+
+
+def test_p1_renamed(p1):
+    """Check renaming Polygon is OK."""
+    p1.rename('YOGA')
+    assert p1.into_euk() == \
+        'box -0.1, -0.1, 3.8, 4.6\n\n'\
+        'A = point(0.5, 0.5)\n'\
+        'G = point(3, 1)\n'\
+        'O = point(3.2, 4)\n'\
+        'Y = point(0.8, 3)\n'\
+        '\n'\
+        'draw\n'\
+        '  (A.G.O.Y)\n'\
+        '  $\\rotatebox{11}{\sffamily 4~cm}$ A 11 - 12.7 deg 4.1\n'\
+        '  $\\rotatebox{86}{\sffamily 3~cm}$ G 86 - 8.9 deg 4.9\n'\
+        '  $\\rotatebox{23}{\sffamily 2~cm}$ O 203 - 12.2 deg 4.2\n'\
+        '  $\\rotatebox{83}{\sffamily 6.5~cm}$ Y 263 - 12.9 deg 4.1\n'\
+        '  $\\rotatebox{47.3}{\sffamily 64\\textdegree}$ A 47.3 deg 2.7\n'\
+        '  $\\rotatebox{-41.3}{\sffamily 128\\textdegree}$ G 138.7 deg 2.7\n'\
+        '  $\\rotatebox{54.3}{\sffamily 32\\textdegree}$ O 234.3 deg 2.7\n'\
+        '  $\\rotatebox{322.7}{\sffamily 256\\textdegree}$ Y 322.7 deg 2.7\n'\
+        '  "A" A 227.3 deg, font("sffamily")\n'\
+        '  "G" G 318.7 deg, font("sffamily")\n'\
+        '  "O" O 54.3 deg, font("sffamily")\n'\
+        '  "Y" Y 142.7 deg, font("sffamily")\n'\
+        'end\n\n'\
+        'label\n'\
+        '  G, A, Y simple\n'\
+        '  O, G, A simple\n'\
+        '  Y, O, G simple\n'\
+        '  A, Y, O simple\n'\
         'end'
