0fe043a3954fcb280d4aa07ac4215b7a726c374f
Nicolas Hainaux <nh.techn@gmail.com>
2017-08-24 16:38:03 +0200

    Add tests for yaml mental calculation sheets.

--- tests/04_sheets/test_mental_calculation_sheets.py
+++ tests/04_sheets/test_mental_calculation_sheets.py
@@ -22,97 +22,88 @@
 
 
 from mathmaker.lib import shared
-from mathmaker.lib.tools.xml import get_xml_sheets_paths
 from mathmaker.lib.document.frames import Sheet
 
-XML_SHEETS = get_xml_sheets_paths()
+
+def test_01_white1_multiplications():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '01_white1',
+                                       'multiplications')))
 
 
-def test_tables2_9():
-    """Checks if 'table_2_9' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['tables2_9'])))
+def test_01_white1_positional_notation():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '01_white1',
+                                       'positional_notation')))
 
 
-def test_divisions():
-    """Checks if 'divisions' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['divisions'])))
+def test_01_white1_exam():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '01_white1',
+                                       'exam')))
 
 
-def test_mini_problems():
-    """Checks if 'mini_problems' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['mini_problems'])))
+def test_02_white2_multiplications():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'multiplications')))
 
 
-def test_multi_11_15_25():
-    """Checks if 'multi_11_15_25' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['multi_11_15_25'])))
+def test_02_white2_addi_subtr():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'addi_subtr')))
 
 
-def test_multi_decimal():
-    """Checks if 'multi_decimal' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['multi_decimal'])))
+def test_02_white2_divisions():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'divisions')))
 
 
-def test_multi_divi_10_100_1000():
-    """Checks if 'multi_divi_10_100_1000' is generated without any error."""
-    shared.machine.write_out(
-        str(Sheet('', '', '', filename=XML_SHEETS['multi_divi_10_100_1000'])))
+def test_02_white2_positional_notation():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'positional_notation')))
 
 
-def test_multi_hole_any_nb():
-    """Checks if 'multi_hole_any_nb' is generated without any error."""
-    shared.machine.write_out(str(
-        Sheet('', '', '', filename=XML_SHEETS['multi_hole_any_nb'])))
+def test_02_white2_fraction_of_a_rectangle():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'fraction_of_a_rectangle')))
 
 
-def test_multi_hole_tables2_9():
-    """Checks if 'multi_hole_tables2_9' is generated without any error."""
-    shared.machine.write_out(
-        str(Sheet('', '', '', filename=XML_SHEETS['multi_hole_tables2_9'])))
+def test_02_white2_mini_problems():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'mini_problems')))
 
 
-def test_multi_reversed():
-    """Checks if 'multi_reversed' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['multi_reversed'])))
+def test_02_white2_multiplication_by_11():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'multiplication_by_11')))
 
 
-def test_operations_vocabulary():
-    """Checks if 'operations_vocabulary' is generated without any error."""
-    shared.machine.write_out(
-        str(Sheet('', '', '', filename=XML_SHEETS['operations_vocabulary'])))
+def test_02_white2_complements():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'complements')))
 
 
-def test_ranks():
-    """Checks if 'ranks' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['ranks'])))
-
-
-def test_rectangles():
-    """Checks if 'rectangles' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['rectangles'])))
-
-
-def test_test_11_1():
-    """Checks if 'test_11_1' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['test_11_1'])))
-
-
-def test_test_11_2():
-    """Checks if 'test_11_2' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['test_11_2'])))
-
-
-def test_test_multi_clever():
-    """Checks if 'multi_clever' is generated without any error."""
-    shared.machine.write_out(str(Sheet('', '', '',
-                                       filename=XML_SHEETS['multi_clever'])))
+def test_02_white2_exam():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '02_white2',
+                                       'exam')))
