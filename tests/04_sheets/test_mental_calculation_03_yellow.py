8d7fe39f6f15aee355d955ce878640c0ced82008
Nicolas Hainaux <nh.techn@gmail.com>
2017-09-21 16:47:46 +0200

    Add tests for the three last new sheets. Split tests for mental calculation in several files.

---
+++ tests/04_sheets/test_mental_calculation_03_yellow.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker creates automatically maths exercises sheets
+# with their answers
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker.
+
+# Mathmaker is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+from mathmaker.lib import shared
+from mathmaker.lib.document.frames import Sheet
+
+
+def test_03_yellow_complements():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'complements')))
+
+
+def test_03_yellow_addi_subtr_hole():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'addi_subtr_hole')))
+
+
+def test_03_yellow_units_conversions():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'units_conversions')))
+
+
+def test_03_yellow_decimal_numerals():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'decimal_numerals')))
+
+
+def test_03_yellow_operations_vocabulary():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'operations_vocabulary')))
+
+
+def test_03_yellow_positional_notation():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'positional_notation')))
+
+
+def test_03_yellow_rectangles():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'rectangles')))
+
+
+def test_03_yellow_multi_15_25():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '03_yellow',
+                                       'multi_15_25')))
