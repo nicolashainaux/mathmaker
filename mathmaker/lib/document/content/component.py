df378a18baa85b1a1d5666276fdf460bb8df0389
Nicolas Hainaux <nh.techn@gmail.com>
2017-10-03 16:11:57 +0200

    Default js answer will be undefined instead of raising a NotImplementedError. All questions cannot have a js answer.

--- mathmaker/lib/document/content/component.py
+++ mathmaker/lib/document/content/component.py
@@ -67,7 +67,7 @@ class structure(object):
 
         Must be reimplemented in each question.
         """
-        raise NotImplementedError
+        return 'undefined'
 
     @property
     def nb_list(self):
