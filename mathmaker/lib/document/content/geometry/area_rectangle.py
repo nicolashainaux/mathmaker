12bc176a6297efb798e76d44cb951ca6a361d9f1
Nicolas Hainaux <nh.techn@gmail.com>
2018-03-22 09:03:14 +0100

    Update area_rectangle question.

--- mathmaker/lib/document/content/geometry/area_rectangle.py
+++ mathmaker/lib/document/content/geometry/area_rectangle.py
@@ -36,13 +36,18 @@ class sub_object(component.structure):
         super().setup("nb_variants", nb=build_data, **options)
         super().setup("length_units", **options)
         super().setup("rectangle", **options)
-        self.transduration = 8
+        self.transduration = 14
 
         if self.picture:
-            self.wording = _("Area of this rectangle? |hint:area_unit|")
+            if self.slideshow:
+                self.wording = _('Area of this rectangle?')
+            else:
+                self.wording = _('Area of this rectangle? |hint:area_unit|')
             setup_wording_format_of(self)
+            self.part2_wording = _('(Length unit: {})')\
+                .format(self.length_unit)
         else:
-            self.transduration = 12
+            self.transduration = 20
             self.nb1, self.nb2 = \
                 self.rectangle.lbl_width, self.rectangle.lbl_length
             self.wording = _("Area of a rectangle whose width is {nb1} \
@@ -52,20 +57,23 @@ class sub_object(component.structure):
     def q(self, **options):
         if self.picture:
             if self.slideshow:
-                return '{}\n{}'\
-                    .format(self.rectangle.drawn,
-                            self.wording.format(**self.wording_format))
+                return r'{}\par {} \par {}'\
+                    .format(self.wording.format(**self.wording_format),
+                            self.rectangle.drawn,
+                            self.part2_wording)
             else:
                 return shared.machine.write_layout(
-                    (1, 2), [5, 8],
+                    (1, 2), [4, 9],
                     [self.rectangle.drawn,
-                     self.wording.format(**self.wording_format)])
+                     r'{} {}'
+                     .format(self.wording.format(**self.wording_format),
+                             self.part2_wording)])
         else:
             return self.wording.format(**self.wording_format)
 
     def a(self, **options):
         # This is actually meant for self.preset == 'mental calculation'
-        return self.rectangle.lbl_area.printed
+        return Number(self.rectangle.lbl_area, unit=self.unit_area).printed
 
     def js_a(self, **kwargs):
         return [Number(self.rectangle.lbl_area, unit=None).uiprinted]
