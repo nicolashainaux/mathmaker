7616569952aaf44f733e6c4124fa3f02bfa3f155
Nicolas Hainaux <nh.techn@gmail.com>
2018-06-12 15:42:54 +0200

    Add percentages training sheet for orange belt.

---
+++ tests/integration/mental_calculation/06_orange/test_06_orange_percentages.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker creates automatically maths exercises sheets
+# with their answers
+# Copyright 2006-2018 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker.
+
+# Mathmaker is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+from mathmaker.lib import shared
+from mathmaker.lib.document.frames import Sheet
+
+
+def test_percentages():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '06_orange',
+                                       'percentages')),
+                             pdf_output=True)
+
+
+def test_percentages_embedding_js():
+    """Check this sheet is generated without any error."""
+    shared.machine.write_out(str(Sheet('mental_calculation',
+                                       '06_orange',
+                                       'percentages',
+                                       enable_js_form=True)),
+                             pdf_output=True)
