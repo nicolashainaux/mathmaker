42ca12a502ab7bf4a251259a27ade22923451e02
Nicolas Hainaux <nh.techn@gmail.com>
2022-11-06 17:15:46 +0100

    Fix and improve (yet unused) numeric_factorization question.

--- mathmaker/lib/document/content/calculation/numeric_factorization.py
+++ mathmaker/lib/document/content/calculation/numeric_factorization.py
@@ -20,9 +20,11 @@
 # along with Mathmaker; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import copy
 import random
 
-from mathmakerlib.calculus import Number, is_integer
+from intspan import intspan
+from mathmakerlib.calculus import Number
 
 from mathmaker.lib import shared
 from mathmaker.lib.constants.latex import COLORED_QUESTION_MARK
@@ -33,40 +35,62 @@ class sub_object(component.structure):
 
     def __init__(self, build_data, **options):
         """
+        The first number of the IntspanProduct, if recognized in the result,
+        will be placed at first position in build_data; the first number in
+        build_data is the one to be split. The possible nb_variant will take
+        effect on the second number only.
+
+        nb_source must be a nnpairs:*
+
+        Options: split_as=
+        dig* (where * is a natural number);
+        unit (to split at unit rank);
+        int_as_halves, int_as_quarters, int_as_halves_or_quarters
+
         :param build_data: data used to build the question, in the form of a
         pair of numbers (a, b). Question will be "Calculate a1 × b + a2 × b"
         where a1 + a2 = a. Factors of each product may be swapped.
         :type build_data: tuple
         """
+        # process build_data to put the first number of the IntspanProduct at
+        # first position
+        ip_1 = intspan(options['nb_source'].split(':')[1].split('×')[0])
+        if build_data[1] in ip_1 and build_data[0] not in ip_1:
+            build_data = (build_data[1], build_data[0])
         super().setup("minimal", **options)
-        # super().setup("nb_variants", nb=build_data, **options)
-        self.transduration = 21
-        if not is_integer(build_data[1]):
-            self.transduration = 30
+        self.transduration = 30
         split_as = options.get('split_as', 'dig1')
         split_options = {'int_as_halves': False,
                          'int_as_quarters': False,
                          'int_as_halves_or_quarters': False}
         if split_as in ['halves', 'quarters', 'halves_or_quarters']:
             split_options.update({'int_as_' + split_as: True})
-        elif split_as == 'dig1':
-            split_options.update({'dig': 1})
+        elif split_as.startswith('dig'):
+            split_options.update({'dig': int(split_as[len('dig'):])})
+        elif split_as == 'unit':
+            split_options.update({'at_unit': True})
         a, b = build_data
         a, b = Number(a), Number(b)
+        # setup possible nb_variant on b only
+        super().setup("numbers", nb=[b], shuffle_nbs=False, **options)
+        super().setup("nb_variants", **options)
+        # then retrieve b, after nb_variant has been applied to it
+        b = copy.deepcopy(self.nb1)
+        # and go on, preparing the numbers
         a1, a2 = a.split(**split_options)
         n1n2 = [a1, b]
         n3n4 = [a2, b]
         if options.get('do_shuffle', True):
             random.shuffle(n1n2)
             random.shuffle(n3n4)
-        n1, n2 = n1n2.pop(), n1n2.pop()
-        n3, n4 = n3n4.pop(), n3n4.pop()
+        n1, n2 = n1n2
+        n3, n4 = n3n4
         super().setup("numbers", nb=[n1, n2, n3, n4], shuffle_nbs=False,
                       **options)
 
-        self.math_expr = r'{} \times {} + {} \times {}'\
-            .format(self.nb1.printed, self.nb2.printed, self.nb3.printed,
-                    self.nb4.printed)
+        self.math_expr = r'{n1} \times {n2} + {n3} \times {n4}'\
+            .format(n1=self.nb1.printed, n2=self.nb2.printed,
+                    n3=self.nb3.printed, n4=self.nb4.printed)
         self.answer = (a * b).standardized()
 
     def q(self, **options):
--- tests/02_questions/test_numeric_factorization.py
+++ tests/02_questions/test_numeric_factorization.py
@@ -22,55 +22,88 @@
 
 # import pytest
 
+from mathmakerlib.calculus import Number
+
 from mathmaker.lib.document.content.calculation import numeric_factorization
 
 
 def test_numeric_factorization():
     """Check all normal cases are correctly handled."""
-    o = numeric_factorization.sub_object(build_data=[2, 7], do_shuffle=False)
+    o = numeric_factorization.sub_object(
+        build_data=[2, 7], do_shuffle=False, nb_source='nnpairs:2-9')
     assert o.q() in [
-        r'$ 7 \times 0.1 + 7 \times 1.9 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.2 + 7 \times 1.8 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.3 + 7 \times 1.7 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.4 + 7 \times 1.6 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.5 + 7 \times 1.5 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.6 + 7 \times 1.4 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.7 + 7 \times 1.3 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.8 + 7 \times 1.2 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.9 + 7 \times 1.1 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.1 + 7 \times 0.9 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.2 + 7 \times 0.8 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.3 + 7 \times 0.7 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.4 + 7 \times 0.6 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.5 + 7 \times 0.5 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.6 + 7 \times 0.4 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.7 + 7 \times 0.3 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.8 + 7 \times 0.2 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.9 + 7 \times 0.1 $ = \textcolor{BrickRed}{\text{?}}'
+        r'$ 0.1 \times 7 + 1.9 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.2 \times 7 + 1.8 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.3 \times 7 + 1.7 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.4 \times 7 + 1.6 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.5 \times 7 + 1.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.6 \times 7 + 1.4 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.7 \times 7 + 1.3 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.8 \times 7 + 1.2 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.9 \times 7 + 1.1 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.1 \times 7 + 0.9 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.2 \times 7 + 0.8 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.3 \times 7 + 0.7 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.4 \times 7 + 0.6 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.5 \times 7 + 0.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.6 \times 7 + 0.4 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.7 \times 7 + 0.3 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.8 \times 7 + 0.2 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.9 \times 7 + 0.1 \times 7 $ = \textcolor{BrickRed}{\text{?}}'
     ]
     assert o.a() == '14'
     assert o.js_a() == ['14']
-    o = numeric_factorization.sub_object(build_data=[2, 7], do_shuffle=False,
-                                         split_as='quarters')
+    o = numeric_factorization.sub_object(
+        build_data=[2, 7], do_shuffle=False, nb_source='nnpairs:2-9',
+        split_as='quarters')
     assert o.q() in [
-        r'$ 7 \times 0.25 + 7 \times 1.75 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.75 + 7 \times 1.25 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.25 + 7 \times 0.75 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.75 + 7 \times 0.25 $ = \textcolor{BrickRed}{\text{?}}'
+        r'$ 0.25 \times 7 + 1.75 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.75 \times 7 + 1.25 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.25 \times 7 + 0.75 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.75 \times 7 + 0.25 \times 7 $ = \textcolor{BrickRed}{\text{?}}'
     ]
     assert o.a() == '14'
     assert o.js_a() == ['14']
-    o = numeric_factorization.sub_object(build_data=[2, 7], split_as='halves')
+    o = numeric_factorization.sub_object(
+        build_data=[2, 7], do_shuffle=False, nb_source='nnpairs:2-9',
+        split_as='halves')
     assert o.a() == '14'
     assert o.js_a() == ['14']
     assert o.nb1 * o.nb2 + o.nb3 * o.nb4 == 14
     assert o.q() in [
-        r'$ 7 \times 0.5 + 7 \times 1.5 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 0.5 + 1.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 0.5 \times 7 + 7 \times 1.5 $ = \textcolor{BrickRed}{\text{?}}',
         r'$ 0.5 \times 7 + 1.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.5 + 7 \times 0.5 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 7 \times 1.5 + 0.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
-        r'$ 1.5 \times 7 + 7 \times 0.5 $ = \textcolor{BrickRed}{\text{?}}',
         r'$ 1.5 \times 7 + 0.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+    ]
+    o = numeric_factorization.sub_object(
+        build_data=[2, 7], do_shuffle=True, nb_source='nnpairs:2-9',
+        split_as='halves')
+    assert o.a() == '14'
+    assert o.js_a() == ['14']
+    assert o.nb1 * o.nb2 + o.nb3 * o.nb4 == 14
+    assert o.q() in [
+        r'$ 0.5 \times 7 + 1.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.5 \times 7 + 0.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 0.5 \times 7 + 7 \times 1.5 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1.5 \times 7 + 7 \times 0.5 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 7 \times 0.5 + 7 \times 1.5 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 7 \times 1.5 + 7 \times 0.5 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 7 \times 0.5 + 1.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 7 \times 1.5 + 0.5 \times 7 $ = \textcolor{BrickRed}{\text{?}}'
+    ]
+    o = numeric_factorization.sub_object(
+        build_data=[16, 10], do_shuffle=False, nb_source='nnpairs:10×11-19',
+        split_as='unit', nb_variant='decimal2')
+    assert o.a() == '1.6'
+    assert o.js_a() == ['1.6']
+    assert o.nb1 * o.nb2 + o.nb3 * o.nb4 == Number('1.6')
+    assert o.q() in [
+        r'$ 9 \times 0.16 + 1 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 8 \times 0.16 + 2 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 7 \times 0.16 + 3 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 6 \times 0.16 + 4 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 5 \times 0.16 + 5 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 4 \times 0.16 + 6 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 3 \times 0.16 + 7 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 2 \times 0.16 + 8 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}',
+        r'$ 1 \times 0.16 + 9 \times 0.16 $ = \textcolor{BrickRed}{\text{?}}'
     ]
