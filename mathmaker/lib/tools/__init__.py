51bb4102fdf3f4307f5ef1b44710a08af3a89f82
Nicolas Hainaux <nh.techn@gmail.com>
2022-12-15 18:04:54 +0100

    Fix bug: divisors() fails if provided a decimal.Decimal (or derived class).

--- mathmaker/lib/tools/__init__.py
+++ mathmaker/lib/tools/__init__.py
@@ -337,7 +337,7 @@ def rotate(L, n):
 
 def divisors(n):
     output = []
-    for i in range(1, int(n ** 0.5) + 1):
+    for i in range(1, int(n ** Number(0.5)) + 1):
         if n % i == 0:
             output.append(i)
             output.append(n // i)
